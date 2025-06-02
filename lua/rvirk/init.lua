c0c3607c7e80ab364ea0bae73991827601ad7707
rupinder <ra14@iitbbs.ac.in>
2025-06-02 10:21:46 -0600

    initial commit

---
+++ lua/rvirk/init.lua
@@ -0,0 +1,3 @@
+require("rvirk.remap")
+require("rvirk.lazy_init")
+require("rvirk.set")
---
+++ lua/rvirk/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
