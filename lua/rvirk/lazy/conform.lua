54cdda4da3cfcbef39575848b23d3071f9f78420
rupinder <ra14@iitbbs.ac.in>
2025-06-02 11:17:13 -0600

    add conform

---
+++ lua/rvirk/lazy/conform.lua
@@ -0,0 +1,17 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                go = { "gofmt" }
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        })
+    end
+}
+
--- lua/rvirk/remap.lua
+++ lua/rvirk/remap.lua
@@ -3,3 +3,10 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>f", function()
+    require("conform").format({ bufnr = 0 })
+end)
